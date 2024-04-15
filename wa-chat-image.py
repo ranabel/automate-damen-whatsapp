b438bf5b2f3274e8066c6a7e59520af5e566a3aa
elava <115636244+ranabel@users.noreply.github.com>
2024-04-16 00:04:02 +0700

    Update wa-chat-image.py

--- wa-chat-image.py
+++ wa-chat-image.py
@@ -1,20 +1,17 @@
 import pywhatkit as pw
 import csv
 
-# Path to the image
 img = 'image.png'  # Ganti dengan path gambar Anda
 
-# Buka file CSV
 with open("data.csv", "r") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
 
-    # Kirim pesan dan gambar ke setiap kontak
     for row in reader:
         number = row[0]
         name = row[1]
 
         msg = f"Selamat Malam {name}!\nNice Dream :)\n<3"
 
-        # pw.sendwhatmsg_instantly(number, msg, 20, True, 5)  # Kirim pesan instan
+        # pw.sendwhatmsg_instantly(number, msg, 30, True, 10)  # Kirim pesan instan
          # disini caption image merupakan pesan yang ingin kita kirimkan
-        pw.sendwhats_image(number, img, caption=msg, tab_close=True, wait_time=13)  # Kirim gambar
+        pw.sendwhats_image(number, img, caption=msg, tab_close=True, wait_time=30)
